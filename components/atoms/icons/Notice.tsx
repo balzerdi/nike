a9c983b827d9e2b4024d7bb6bbb39dd0b24ba213
Dimitri Balzer <38956494+balzerdi@users.noreply.github.com>
2022-08-28 22:51:33 +0200

    Bugfix

--- components/atoms/icons/Notice.tsx
+++ components/atoms/icons/Notice.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Turn from '../../../public/images/turn_your_phone.png'
 
 const Container = styled.div`
-    position: absolute;
+    position: fixed;
     top: 0;
     right: 0;
     bottom: 0;
